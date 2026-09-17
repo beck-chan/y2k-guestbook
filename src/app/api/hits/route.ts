cc19cdb1c9dcec5652088943bdd17c5b93a2fec3
Beck Chan <rebecca.ht.chan@gmail.com>
2026-09-17 14:17:08 -0700

    UGH

--- src/app/api/hits/route.ts
+++ src/app/api/hits/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 import { getUniqueVisitors } from "../../../lib/uniqueVisitors";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const count = await getUniqueVisitors();
   return NextResponse.json({ count });
