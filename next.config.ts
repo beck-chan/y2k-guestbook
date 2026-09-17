07588d80502c4f76246ff17a61faeca0c4a3f161
Beck Chan <rebecca.ht.chan@gmail.com>
2026-09-16 19:36:43 -0700

    save

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 import { withGuestbookConfig } from "./src/lib/withGuestbookConfig";
 
 const nextConfig: NextConfig = {
-  agentRules: false,
   env: {
     FLAG_COUNTER: process.env.FLAG_COUNTER,
   },
